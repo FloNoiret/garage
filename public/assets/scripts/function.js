7cc20358f247d2dff14f4f827bd113179d425200
Flora Noiret <flo.n@hotmail.fr>
2023-07-16 18:08:41 +0200

    Adjust car form subject according to car filtered

--- public/assets/scripts/function.js
+++ public/assets/scripts/function.js
@@ -12,8 +12,6 @@ document.addEventListener('DOMContentLoaded', function CarTitle() {
 
     // Set the id dynamically
     carTitle.setAttribute("id", "car-title" + i);
-
-
   }
 
   // Add dynamic id to subject input
@@ -29,7 +27,40 @@ document.addEventListener('DOMContentLoaded', function CarTitle() {
     carContact.setAttribute("type", "hidden");
     carContact.value = carTitles[i].innerHTML;
   }
+  let filterbtn = document.getElementById("filterbtn")
+
+  document.addEventListener('click', function CarTitleNew() {
+    //Add dynamic id to car titles
+    //Get the Titles
+    var carTitlesNew = document.querySelectorAll('h2');
+  
+    // Looping over Title
+    for (var j = 0; j < carTitlesNew.length; j++) {
+  
+      // Get the ith Title
+      var carTitleNew = carTitlesNew[j];
+  
+      // Set the id dynamically
+      carTitleNew.setAttribute("id", "car-title" + j);
+    }
+  
+    // Add dynamic id to subject input
+    var carContactsNew = document.querySelectorAll('input[name="contact[subject]"]');
+    // Looping over Title
+    for (var j = 0; j < carTitlesNew.length; j++) {
+  
+      // Get the ith Title
+      var carContactNew = carContactsNew[j];
+  
+      // Set the id dynamically
+      carContactNew.setAttribute("id", "car-subject" + j);
+      carContactNew.setAttribute("type", "hidden");
+      carContactNew.value = carTitlesNew[j].innerHTML;
+    }
+  })
+  
 })
+
 
 
 
