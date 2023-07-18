c0ffe5a8ddcf183f5d2a00e5e9b605900436dd20
Flora Noiret <flo.n@hotmail.fr>
2023-07-18 14:55:30 +0200

    Create TimeTable Fixtures and clean code

--- src/Controller/TimeController.php
+++ src/Controller/TimeController.php
@@ -12,26 +12,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TimeController extends AbstractController
 {
-    /* Route & Controller to get first data 
-    #[Route('/timetable')]
-    public function create(Request $request, ManagerRegistry $doctrine): Response
-    {
-        $this->denyAccessUnlessGranted("ROLE_ADMIN");
-        $timetable = new TimeTable();
-        $timetable_form = $this->createForm(TimeTableType::class, $timetable);
-        $timetable_form->handleRequest($request);
-
-        if ($timetable_form->isSubmitted() && $timetable_form->isValid()) {
-            $entityManager = $doctrine->getManager();
-            $entityManager->persist($timetable);
-            $entityManager->flush();
-            return $this->redirectToRoute("accueil");
-        }
-        return $this->render('time/form.html.twig', [
-            "timetable_form" => $timetable_form->createView()
-        ]);
-    }*/
-
     /*  Modify data*/
     #[Route('/time/edit/{id<\d+>}', name: "edit-time")]
     public function updateTimeTable(Request $request, TimeTable $timetable, ManagerRegistry $doctrine): Response
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,7 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\TimeTable;
 use App\Entity\User;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -19,10 +21,50 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-
+        // User admin creation
         $adminUser = new User($this->passwordHasher);
         $adminUser->setUsername($_ENV['ADMIN_ID'])->setPassword($_ENV['ADMIN_PASSWORD'])->setRoles(array('ROLE_ADMIN'));
         $manager->persist($adminUser);
+
+        // Timetable creation
+        $timetable = new TimeTable();
+        $timetable
+        ->setMondayAM(new \DateTime('08:00:00'))
+        ->setClosemondayAM(new \DateTime('12:00:00'))
+        ->setMondayPM(new \DateTime('14:00:00'))
+        ->setClosemondayPM(new \DateTime('18:00:00'))
+
+        ->setTuesdayAM(new \DateTime('08:00:00'))
+        ->setClosetuesdayAM(new \DateTime('12:00:00'))
+        ->setTuesdayPM(new \DateTime('14:00:00'))
+        ->setClosetuesdayPM(new \DateTime('18:00:00'))
+
+        ->setWednesdayAM(new \DateTime('07:30:00'))
+        ->setClosewednesdayAM(new \DateTime('12:30:00'))
+        ->setWednesdayPM(new \DateTime('13:00:00'))
+        ->setClosewednesdayPM(new \DateTime('19:00:00'))
+
+        ->setThursdayAM(new \DateTime('08:00:00'))
+        ->setClosethursdayAM(new \DateTime('12:00:00'))
+        ->setThursdayPM(new \DateTime('14:00:00'))
+        ->setClosethursdayPM(new \DateTime('18:00:00'))
+
+        ->setFridayAM(new \DateTime('07:30:00'))
+        ->setClosefridayAM(new \DateTime('12:30:00'))
+        ->setFridayPM(new \DateTime('13:00:00'))
+        ->setClosefridayPM(new \DateTime('20:00:00'))
+
+        ->setSaturdayAM(new \DateTime('07:00:00'))
+        ->setClosesaturdayAM(new \DateTime('12:30:00'))
+        ->setSaturdayPM(new \DateTime('13:00:00'))
+        ->setClosesaturdayPM(new \DateTime('20:00:00'))
+
+        ->setSundayAM(new \DateTime('00:00:00'))
+        ->setClosesundayAM(new \DateTime('00:00:00'))
+        ->setSundayPM(new \DateTime('00:00:00'))
+        ->setCloseSundayPM(new \DateTime('00:00:00'));
+
+        $manager->persist($timetable);
 
         $manager->flush();
     }
